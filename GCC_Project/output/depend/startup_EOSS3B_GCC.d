1109529be727bc4f43bfdc7cb08988ab4443fe8b
MINIAMD\lynxe <lynxeyed@live.jp>
2021-07-27 02:51:17 +0900

    update

--- GCC_Project/output/depend/startup_EOSS3B_GCC.d
+++
@@ -1,2 +0,0 @@
-/mnt/c/qorc-sdk/qf_apps/qf_wbfpga_pio/GCC_Project/output/startup_EOSS3B_GCC.o: \
- /mnt/c/qorc-sdk/qf_apps/qf_wbfpga_pio/GCC_Project/../../../HAL/startup/startup_EOSS3B_GCC.c
--- GCC_Project/output/depend/vectors_CM4F_gcc.d
+++
@@ -1,2 +0,0 @@
-/mnt/c/qorc-sdk/qf_apps/qf_wbfpga_pio/GCC_Project/output/vectors_CM4F_gcc.o: \
- /mnt/c/qorc-sdk/qf_apps/qf_wbfpga_pio/GCC_Project/../../../HAL/startup/vectors_CM4F_gcc.c
