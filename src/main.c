7bb1fac0b9a47c9ecda6f19400a81e6eb494d78f
panda5mt <lynxeyed@live.jp>
2021-08-21 23:41:28 +0900

    automatically uploaded at 20210821_234128

--- src/main.c
+++ src/main.c
@@ -379,7 +379,7 @@ int main(void)
 //     xTaskSet_uSecCount(1546300800ULL * 1000ULL * 1000ULL); // start at 2019-01-01 00:00:00 UTC time
 //     /* Start the tasks and timer running. */
 //     vTaskStartScheduler();
-//     dbg_str("\n");
+
     while(1) ;
 }
 
