562c24aac666e9cdf478115464fe1c21dc9b0471
Atsushi Saitoh <>
2021-09-10 05:03:54 +0000

    Automatically uploaded

--- src/qf_hardwaresetup.c
+++ src/qf_hardwaresetup.c
@@ -100,7 +100,7 @@ static void uart_setup()
     memset( (void *)&(uartObj), 0, sizeof(uartObj) );
 
     uart_id = UART_ID_HW;
-	brate = BAUD_115200;//BAUD_921600;
+	brate = BAUD_115200;
 	uartObj.baud = brate;
 	uartObj.wl = WORDLEN_8B;
 	uartObj.parity = PARITY_NONE;
