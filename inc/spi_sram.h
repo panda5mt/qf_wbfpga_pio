08d2ae0c98c15636dfd2630694915ae6066eda3b
Atsushi Saitoh <>
2021-09-26 08:32:04 +0000

    Automatically uploaded

--- inc/spi_sram.h
+++ inc/spi_sram.h
@@ -13,6 +13,19 @@ extern "C" {
 HAL_StatusTypeDef spi0_sram_init(void);
 HAL_StatusTypeDef spi0_sram_trans(uint8_t *cmd, uint32_t cmd_len) ;
 
+// See TRM p.245~p.252 "SPI_0_Master registers"
+
+// SPI_0_Master Offsets
+#define SPI0_BAUD_LSB   (0x00)
+#define SPI0_BAUD_MSB   (0x01)
+#define SPI0_CONFIG     (0x02)
+#define SPI0_TRAN_RECV  (0x03)
+#define SPI0_CSR        (0x04)
+#define SPI0_SLAVE_SEL  (0x05)
+#define SPI0_CLK_CTRL   (0x06)
+#define SPI0_CLK_DEACT  (0x07)
+
+
 
 
 #ifdef __cplusplus
