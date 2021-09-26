df3753ed666be46d2745454c9e16e4d6f088cff0
Atsushi Saitoh <>
2021-09-26 08:32:30 +0000

    Automatically uploaded

--- inc/spi_sram.h
+++ inc/spi_sram.h
@@ -8,11 +8,6 @@ extern "C" {
 #include "eoss3_dev.h"
 #include "eoss3_hal_pad_config.h"
 #include "eoss3_hal_wb.h"
-
-
-HAL_StatusTypeDef spi0_sram_init(void);
-HAL_StatusTypeDef spi0_sram_trans(uint8_t *cmd, uint32_t cmd_len) ;
-
 // See TRM p.245~p.252 "SPI_0_Master registers"
 
 // SPI_0_Master Offsets
@@ -25,8 +20,8 @@ HAL_StatusTypeDef spi0_sram_trans(uint8_t *cmd, uint32_t cmd_len) ;
 #define SPI0_CLK_CTRL   (0x06)
 #define SPI0_CLK_DEACT  (0x07)
 
-
-
+HAL_StatusTypeDef spi0_sram_init(void);
+HAL_StatusTypeDef spi0_sram_trans(uint8_t *cmd, uint32_t cmd_len) ;
 
 #ifdef __cplusplus
   }
